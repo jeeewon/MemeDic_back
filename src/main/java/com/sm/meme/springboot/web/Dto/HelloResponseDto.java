fa77d3ec4482da2e9630c61f779c6adedc66ba1b
jeeewon <jiwon9586@gmail.com>
2022-04-22 16:31:47 +0900

    mvc_error

--- src/main/java/com/sm/meme/springboot/web/Dto/HelloResponseDto.java
+++ src/main/java/com/sm/meme/springboot/web/Dto/HelloResponseDto.java
@@ -1,4 +1,12 @@
 package com.sm.meme.springboot.web.Dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+
 public class HelloResponseDto {
+    private final String name;
+    private final int amount;
 }
